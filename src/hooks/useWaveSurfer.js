ce7abf8ccddbd790134b176fe3830aeeb2f71404
Moh <Moh.gmail.com>
2022-10-05 19:03:53 +0300

    updates

--- src/hooks/useWaveSurfer.js
+++ src/hooks/useWaveSurfer.js
@@ -5,27 +5,59 @@ import { useEffect, useState } from "react";
 export const useWaveSurfer = ({ url, wavesurfer, waveformRef }) => {
   const [playing, setPlaying] = useState(false);
   const [volume, setVolume] = useState(0.5);
-
+  const [panner, setPanner] = useState(0);
+  const [leftGain, setLeftGain] = useState();
+  const [rightGain, setRightGain] = useState();
+  let _waveSurfer = {};
   useEffect(() => {
     setPlaying(false);
     const options = formWaveSurferOptions(waveformRef.current);
-    wavesurfer.current = WaveSurfer.create(options);
-    fetch(url)
-      .then((e) => {
-        return e.blob();
-      })
-      .then((e) => {
-        wavesurfer.current.loadBlob(e);
-      });
+    _waveSurfer = WaveSurfer.create(options);
+    _waveSurfer.panner = _waveSurfer.backend.ac.createStereoPanner();
+    const channelSplitterNode = _waveSurfer.backend.ac.createChannelSplitter(2);
+    const channelMergerNode = _waveSurfer.backend.ac.createChannelMerger(2);
+    const leftGainNode = _waveSurfer.backend.ac.createGain();
+    const rightGainNode = _waveSurfer.backend.ac.createGain();
+    channelSplitterNode.connect(leftGainNode, 0);
+    leftGainNode.gain.value = 0.8;
+    channelSplitterNode.connect(rightGainNode, 1);
+    rightGainNode.gain.value = 0.8;
+    leftGainNode.connect(channelMergerNode, 0, 0);
+    rightGainNode.connect(channelMergerNode, 0, 1);
+    _waveSurfer.backend.setFilters([
+      channelSplitterNode,
+      leftGainNode,
+      channelMergerNode,
+      _waveSurfer.panner,
+    ]);
+    wavesurfer.current = _waveSurfer;
 
-    wavesurfer.current.on("ready", function () {
-      if (wavesurfer.current) {
+    let audio = document.createElement("audio");
+    audio.src = url;
+    audio.crossOrigin = "anonymous";
+    wavesurfer.current.load(audio);
+    wavesurfer?.current?.on("ready", function () {
+      if (wavesurfer?.current) {
         wavesurfer.current.setVolume(volume);
-        setVolume(volume);
       }
     });
 
-    return () => wavesurfer.current.destroy();
-  }, [url, volume, waveformRef, wavesurfer]);
-  return { volume, setVolume, playing, setPlaying };
+    setLeftGain(leftGainNode);
+    setRightGain(rightGainNode);
+
+    return () => {
+      return wavesurfer.current.destroy();
+    };
+  }, [url, waveformRef, wavesurfer]);
+
+  return {
+    volume,
+    setVolume,
+    playing,
+    setPlaying,
+    panner,
+    setPanner,
+    leftGain,
+    rightGain,
+  };
 };
--- src/lib/formWaveSurferOptions.js
+++ src/lib/formWaveSurferOptions.js
@@ -1,5 +1,6 @@
 export const formWaveSurferOptions = (ref) => ({
   container: ref,
+
   waveColor: "#0d71fdb3",
   progressColor: "#0d6efd",
   cursorColor: "OrangeRed",
@@ -7,6 +8,7 @@ export const formWaveSurferOptions = (ref) => ({
   barRadius: 3,
   responsive: true,
   height: 50,
+  splitChannels: true,
   normalize: true,
-  partialRender: true,
+  backend: "MediaElementWebAudio",
 });
